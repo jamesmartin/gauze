32d7b9b35a579d9bf38b8de49a3a0c25930bcf04
James Martin <jamesmartin@github.com>
2017-05-05 10:23:07 +1000

    Polyfill for Element.matches(), for IE's benefit

--- gauze.js
+++ gauze.js
@@ -156,3 +156,20 @@
   // Public interface
   gauze.bindFilterButtons = bindFilterButtons
 }(window._gauze = window._gauze || {}, window, document))
+
+// Element.matches() polyfill (for Internet Explorer)
+// https://developer.mozilla.org/en/docs/Web/API/Element/matches
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function(s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;
+        };
+}
